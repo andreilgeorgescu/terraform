5947d744319b28d68ce418312e565451e8910b35
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-01-21 04:06:11 +0000

    Add

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/tmp/gcp_keyfile.json")
+
+  project = var.project_id
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}

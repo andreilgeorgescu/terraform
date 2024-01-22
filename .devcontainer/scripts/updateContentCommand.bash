902dd3d29f3fa526621d38231166482b674402e1
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-01-22 00:01:31 +0000

    Add Terraform and Terragrunt to .gitignore

--- .devcontainer/scripts/updateContentCommand.bash
+++ .devcontainer/scripts/updateContentCommand.bash
@@ -1,2 +1,5 @@
-curl https://www.toptal.com/developers/gitignore/api/terraform,terragrunt >> .gitignore
+{
+  echo "\n"
+  curl https://www.toptal.com/developers/gitignore/api/terraform,terragrunt
+} >> .gitignore
 terraform init

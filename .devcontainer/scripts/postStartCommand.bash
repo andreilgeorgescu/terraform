30f4cbd89bd7e5b8bb6b1ce92146f675c221445f
Andrei Liviu Georgescu <146103342+andreilgeorgescu@users.noreply.github.com>
2024-01-22 02:16:36 +0000

    Update Terraform configuration and environment variables

--- .devcontainer/scripts/postStartCommand.bash
+++ .devcontainer/scripts/postStartCommand.bash
@@ -6,3 +6,5 @@ echo "$GCP_SERVICE_ACCOUNT_KEY" | base64 --decode >"$gcp_keyfile_path"
 	echo "gcp_keyfile_path = \"$gcp_keyfile_path\""
 	echo "gcp_project_id = \"$GCP_PROJECT_ID\""
 } >terraform.tfvars
+
+export "$TF_TOKEN_APP_TERRAFORM_IO"="$TF_TOKEN_app_terraform_io"
--- main.tf
+++ main.tf
@@ -1,38 +1,26 @@
 terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.12"
+  cloud {
+    organization = "riverwalk-software"
+
+    workspaces {
+      name = "test-cli"
     }
   }
 }
 
-provider "google" {
-  credentials = file(var.gcp_keyfile_path)
+# terraform {
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "~> 5.12"
+#     }
+#   }
+# }
 
-  project = var.gcp_project_id
-  region  = var.region
-  zone    = var.zone
-}
+# provider "google" {
+#   credentials = file(var.gcp_keyfile_path)
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-  tags         = ["web", "dev"]
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
-}
+#   project = var.gcp_project_id
+#   region  = var.region
+#   zone    = var.zone
+# }
